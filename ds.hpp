a365abf1014b235983b84d2acbdcfa62b53d84b6
sel68 <saadhasanellahie@gmail.com>
2025-11-27 01:20:44 +0500

    refactored

--- ds.hpp
+++ ds.hpp
@@ -1,65 +1,133 @@
 #pragma once
 #include <cstddef>
+#include<iostream>
+using namespace std;
 
 namespace ds {
 
 // Singly Linked List
-template<typename T>
 class SList {
-
 private:
     struct Node {
-        T data;
+        int data;
         Node* next;
-        Node(const T& d, Node* n) : data(d), next(n) {}
-        Node(T&& d, Node* n) : data(std::move(d)), next(n) {}
+        Node(int val) : data(val), next(nullptr) {}
     };
-
-    Node* head_;
-    std::size_t sz_;
-
+    Node* head;
+    Node* tail;
+    int size;
 public:
-    SList() noexcept;
-    ~SList() noexcept;
-
-    void push_front(const T& v);
-    void push_front(T&& v);
-
-    void pop_front();
-
-    T& front();
-    const T& front() const;
-
-    bool empty() const noexcept;
-    std::size_t size() const noexcept;
-
+    SList() : head(nullptr), tail(nullptr), size(0) {}
+    ~SList() {
+        Node* current = head;
+        while (current) {
+            Node* nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+    }
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+    void pop_back() {
+        if (!head) return;
+        if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+        } else {
+            Node* current = head;
+            while (current->next != tail) {
+                current = current->next;
+            }
+            delete tail;
+            tail = current;
+            tail->next = nullptr;
+        }
+        size--;
+    }
+    int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node* current = head;
+        for (int i = 0; i < index; i++) {
+            current = current->next;
+        }
+        return current->data;
+    }
+    void set(int index, int value) {
+        if (index < 0 || index >= size) return;
+        Node* current = head;
+        for (int i = 0; i < index; i++) {
+            current = current->next;
+        }
+        current->data = value;
+    }
+    int length() { return size; }
+    bool empty() { return size == 0; }
+    void print() {
+        Node* current = head;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
 };
 
 //Dynamic Array
-template<typename T>
-class DynArray {
+class DynamicArray {
 private:
-    T* data_;
-    std::size_t sz_;
-    std::size_t cap_;
+    int* arr;
+    int capacity;
+    int size;
+
+    void resize() {
+        capacity *= 2;
+        int* newArr = new int[capacity];
+        for (int i = 0; i < size; i++) newArr[i] = arr[i];
+        delete[] arr;
+        arr = newArr;
+    }
 
 public:
-    DynArray() noexcept;
-    explicit DynArray(std::size_t n);
-    ~DynArray() noexcept;
+    DynamicArray(int cap = 2) : capacity(cap), size(0) {
+        arr = new int[capacity];
+    }
 
-    void push_back(const T& v);
-    void push_back(T&& v);
+    ~DynamicArray() {
+        delete[] arr;
+    }
 
-    void pop_back();
+    void push(int value) {
+        if (size == capacity) resize();
+        arr[size++] = value;
+    }
 
-    T& operator[](std::size_t i);
-    const T& operator[](std::size_t i) const;
+    void pop() {
+        if (size > 0) size--;
+    }
 
-    std::size_t size() const noexcept;
-    std::size_t capacity() const noexcept;
-    void reserve(std::size_t new_cap);
+    int get(int index) {
+        return (index >= 0 && index < size) ? arr[index] : -1;
+    }
 
+    void set(int index, int value) {
+        if (index >= 0 && index < size) arr[index] = value;
+    }
+
+    int length() { return size; }
+    bool empty() { return size == 0; }
+
+    void print() {
+        for (int i = 0; i < size; i++) cout << arr[i] << " ";
+        cout << endl;
+    }
 };
 
 }
